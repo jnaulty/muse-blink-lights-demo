d476c33407f575ca609305354f087c6d63c2a74a
jnaulty <jnaulty@gmail.com>
2016-11-10 23:06:34 -0800

    add arduino script

---
+++ led_blink.ino
@@ -0,0 +1,80 @@
+/*
+  Physical Pixel
+
+ An example of using the Arduino board to receive data from the
+ computer.  In this case, the Arduino boards turns on an LED when
+ it receives the character 'H', and turns off the LED when it
+ receives the character 'L'.
+
+ The data can be sent from the Arduino serial monitor, or another
+ program like Processing (see code below), Flash (via a serial-net
+ proxy), PD, or Max/MSP.
+
+ The circuit:
+ * LED connected from digital pin 13 to ground
+
+ created 2006
+ by David A. Mellis
+ modified 30 Aug 2011
+ by Tom Igoe and Scott Fitzgerald
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/PhysicalPixel
+ */
+
+const int ledPin = 13; // the pin that the LED is attached to
+int incomingByte;      // a variable to read incoming serial data into
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // see if there's incoming serial data:
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'G') {
+      Serial.println('G');
+      digitalWrite(ledPin, HIGH);
+      delay(25);
+      digitalWrite(ledPin, LOW);
+      delay(25);
+    }
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'B') {
+      Serial.println('L');
+      digitalWrite(ledPin, HIGH);
+      delay(33);
+      digitalWrite(ledPin, LOW);
+      delay(33);
+    }
+    if (incomingByte == 'D') {
+      Serial.println('D');
+      digitalWrite(ledPin, HIGH);
+      delay(3000);
+      digitalWrite(ledPin, LOW);
+      delay(100);
+    }
+    if (incomingByte == 'A') {
+      Serial.println('A');
+      digitalWrite(ledPin, HIGH);
+      delay(50);
+      digitalWrite(ledPin, LOW);
+      delay(50);
+    }
+    if (incomingByte == 'T') {
+      Serial.println('T');
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+      delay(100);
+    }
+  }
+}
+
