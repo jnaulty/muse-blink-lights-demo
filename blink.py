69f0332cc50d1289233e684de25f0b979e6b47b9
jnaulty <jnaulty@gmail.com>
2016-11-19 12:39:29 -0800

    add gitignore and serial logic

--- blink.py
+++ blink.py
@@ -1,5 +1,5 @@
 import argparse
-import math
+import serial
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
@@ -7,9 +7,12 @@ from pythonosc import osc_server
 
 def blink_handler(unused_addr, args, blink):
     print("Blink: {}".format(blink))
+    ser.write('B')
+
 
 def jaw_handler(unused_addr, args, jaw):
     print("Jaw Clench: {}".format(jaw))
+    ser.write('J')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -20,12 +23,19 @@ if __name__ == "__main__":
                         type=int,
                         default=5000,
                         help="The port to listen on")
+    parser.add_argument("--serial",
+                        default="",
+                        help="Arduino serial port")
     args = parser.parse_args()
+
+    if args.serial:
+        arduino = serial.Serial(args.serial)
 
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/debug", print)
     dispatcher.map("/muse/elements/blink", blink_handler, "Blink")
     dispatcher.map("/muse/elements/jaw_clench", jaw_handler, "Jaw")
+
 
     server = osc_server.ThreadingOSCUDPServer(
         (args.ip, args.port), dispatcher)
