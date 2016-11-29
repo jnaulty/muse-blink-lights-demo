8e0fa965a0e861d779e5f7a1ad4b88378c8a8ddc
jnaulty <jnaulty@gmail.com>
2016-11-28 20:19:10 -0800

    slight modification to blink.py

--- muse-sdk-example.py
+++ muse-sdk-example.py
@@ -1,26 +1,12 @@
 import argparse
-import serial
+import math
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
 
-def write_serial(letter):
-    if SERIAL:
-        arduino.write(letter)
-    else:
-        print(letter)
-
 def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
         print("EEG (uV) per channel: ", ch1, ch2, ch3, ch4)
-
-def blink_handler(unused_addr, args, blink):
-    print("Blink: {}".format(blink))
-    write_serial('B')
-
-def jaw_handler(unused_addr, args, jaw):
-    print("Jaw Clench: {}".format(jaw))
-    write_serial('J')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -31,24 +17,15 @@ if __name__ == "__main__":
                         type=int,
                         default=5000,
                         help="The port to listen on")
-    parser.add_argument("--serial",
-                        default="",
-                        help="Arduino serial port")
     args = parser.parse_args()
-
-    if args.serial:
-        print ('setting up serial device at %s' % args.serial)
-        SERIAL = True
-        arduino = serial.Serial(args.serial)
 
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/debug", print)
-    dispatcher.map("/muse/elements/blink", blink_handler, "Blink")
-    dispatcher.map("/muse/elements/jaw_clench", jaw_handler, "Jaw")
     dispatcher.map("/muse/eeg", eeg_handler, "EEG")
 
-
     server = osc_server.ThreadingOSCUDPServer(
-        (args.ip, args.port), dispatcher)
+    (args.ip, args.port), dispatcher)
     print("Serving on {}".format(server.server_address))
     server.serve_forever()
+
+
